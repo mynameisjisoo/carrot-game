3efff1329ee3fa12539f773b48be501a5a8f1096
mynameisjisoo <devjisoolee@gmail.com>
2021-09-09 20:48:33 +0900

    타이머 메소드 생성, target 만드는 메소드 변경 * setInterval이용하여 timer 역할하는 countDown 메소드 생성함 * target생성하는 메소드(makeTarget)와 target 위치 지정하는 메소드(changeArrangement) 분리함 * headerBtn 변수이름 변경하고 토글 기능 추가함

--- main.js
+++ main.js
@@ -1,52 +1,91 @@
 'use strict';
 
 const section = document.querySelector('.container');
-const playBtn = document.querySelector('.header__play');
-const timer = document.querySelector('.header__timer');
+const main = document.querySelector('.main');
+const headerBtn = document.querySelector('.header__button');
+const headerTimer = document.querySelector('.header__timer');
 const remain = document.querySelector('.header__remain');
-// const main = document.querySelector('.main');
 const redo = document.querySelector('.redo');
-let timeCnt = 10;
-let counter = setInterval(timeCnt, 1000);
 
-// function timecount() {
-//   timeCnt--;
+function handlePlayBtn() {
+  headerBtn.classList.toggle('pause');
+  const pauseBtn = document.querySelector('.pause');
+  if (pauseBtn) {
+    pauseBtn.innerHTML = `<i class="fas fa-stop"></i>`;
+  } else {
+    headerBtn.innerHTML = `<i class="fas fa-play"></i>`;
+  }
+}
 
-//   if (timeCnt <= 0) {
-//     clearInterval(counter); //setinterval로 설정한 작업 취소
-
-//     //타이머끝나면 동작하는 코드?
-//     console.log('끝');
-//   }
-//   header_timer.innerHTML = timeCnt;
-// }
-
-playBtn.addEventListener('click', () => {
-  gamePlay();
+headerBtn.addEventListener('click', () => {
+  countDown();
 });
 
+function countDown() {
+  let remainTime = 10;
+  let timer;
+
+  function showRemaining() {
+    if (remainTime === 0) {
+      clearInterval(timer); //함수 멈춤
+      alert('타이머 종료'); //시간종료하면 나오는 창 띄우기
+      return;
+    }
+    remainTime--;
+    headerTimer.innerText = `00:${remainTime}`;
+  }
+  timer = setInterval(showRemaining, 1000); //1초에 한번 함수 실행
+}
+
+function show
+
+
+makeTarget();
+
 function gamePlay() {
-  makeTarget();
+  for (let i = 0; i < 7; i++) {
+    const bug = document.querySelector(`.bug[data-bug="${i}"]`);
+    changeArrangement(bug);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const carrot = document.querySelector(`.carrot[data-carrot="${i}"]`);
+    changeArrangement(carrot);
+  }
+}
+
+function changeArrangement(element) {
+  // console.log(element);
+  if (element.dataset.bug) {
+    let randomX = Math.floor(Math.random() * 770);
+    let randomY = Math.floor(Math.random() * 220);
+    element.style.transform = `translate(${randomX}px, ${randomY}px)`;
+  } else if (element.dataset.carrot) {
+    for (let i = 0; i < 10; i++) {
+      let randomX = Math.floor(Math.random() * 760);
+      let randomY = Math.floor(Math.random() * 185);
+      element.style.transform = `translate(${randomX}px, ${randomY}px)`;
+      element.style.visibility = '';
+    }
+  }
 }
 
 function makeTarget() {
-  const main = document.createElement('main');
-  main.setAttribute('class', 'main');
-  section.appendChild(main);
-
   for (let i = 0; i < 7; i++) {
     const element = document.createElement('img');
     element.setAttribute('class', 'bug');
     element.setAttribute('src', './img/bug.png');
     element.setAttribute('data-bug', i);
-    main.appendChild(element);
 
     let randomX = Math.floor(Math.random() * 770);
     let randomY = Math.floor(Math.random() * 220);
+    element.style.transform = `translate(${randomX}px, ${randomY}px)`;
 
-    let bugTarget = document.querySelector(`.bug[data-bug="${i}"]`);
+    main.appendChild(element);
+    // changeArrangement(element, i);
+    // let bugTarget = document.querySelector(`.bug[data-bug="${i}"]`);
 
-    bugTarget.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    // bugTarget.style.transform = `translate(${randomX}px, ${randomY}px)`;
   }
 
   for (let i = 0; i < 10; i++) {
@@ -63,31 +102,33 @@ function makeTarget() {
 
     carrotTarget.style.transform = `translate(${randomX}px, ${randomY}px)`;
   }
-  //   section.appendChild(main);
-
-  const tmp = onclick(main);
 }
-// makeTarget();
+
+
 
 let carrotCnt = 10;
 remain.innerText = carrotCnt;
 
-function onclick(main) {
-  main.addEventListener('click', event => {
-    const bug = event.target.dataset.bug;
-    const carrot = event.target.dataset.carrot;
+main.addEventListener('click', event => {
+  const bug = event.target.dataset.bug;
+  const carrot = event.target.dataset.carrot;
 
-    if (bug) {
-      console.log('you lose');
-    } else if (carrot) {
-      carrotCnt--;
-      remain.innerText = carrotCnt;
-      const clickedCarrot = document.querySelector(
-        `.carrot[data-carrot="${carrot}"]`
-      );
-      clickedCarrot.remove();
+  if (bug) {
+    console.log('you lose');
+    lose(carrotCnt);
+  } else if (carrot) {
+    carrotCnt--;
+    remain.innerText = carrotCnt;
+    const clickedCarrot = document.querySelector(
+      `.carrot[data-carrot="${carrot}"]`
+    );
+    clickedCarrot.style.visibility = 'hidden';
+    if (carrotCnt === 0) {
+      alert('끝!');
     }
-  });
-}
+  }
+});
 
-function lose(carrotCnt) {}
+function lose(carrotCnt) {
+  alert(`${carrotCnt}남았다`);
+}
